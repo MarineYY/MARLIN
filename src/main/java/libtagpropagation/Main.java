c0b9a7ea4bb17893a4c4e9c10cd95a35e90b3a3d
wyyanhui <137664580+wyyanhui@users.noreply.github.com>
2024-04-27 21:50:27 +0800

    to add properties of kafka topics into project

--- src/main/java/libtagpropagation/Main.java
+++ src/main/java/libtagpropagation/Main.java
@@ -2,25 +2,21 @@ package libtagpropagation;
 
 import com.twitter.chill.protobuf.ProtobufSerializer;
 import libtagpropagation.graphalignment.GraphAlignmentProcessFunction;
-import org.apache.flink.connector.file.src.FileSource;
-import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
-import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import provenancegraph.AssociatedEvent;
 import provenancegraph.datamodel.PDM;
-import provenancegraph.parser.LocalParser;
+
 import provenancegraph.parser.PDMParser;
+import utils.KafkaConfig;
 import utils.KafkaPDMDeserializer;
 
-import java.util.Objects;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -28,28 +24,13 @@ public class Main {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStream<AssociatedEvent> event_stream;
 
-        if (Objects.equals(args[0], "online")){
-            String kafkaBroker = "192.168.10.110:9092";
-//            String kafkaTopic = "topic-CADETS-0";
-//            String kafkaTopic = "topic-CADETS-1";
-//            String kafkaTopic = "topic-CADETS-2";
-//            String kafkaTopic = "topic-FD-2";
-//            String kafkaTopic = "topic-FD-3";
-//            String kafkaTopic = "topic-THEIA-1r";
-//            String kafkaTopic = "topic-THEIA-3";
-//            String kafkaTopic = "topic-THEIA-5m"   ;
-//            String kafkaTopic = "topic-THEIA-6r";
-            String kafkaTopic = "topic-win10-malicious";
-//            String kafkaTopic = "topic-win10";
-//            String kafkaTopic = "topic-winserver-malicious";
-//            String kafkaTopic = "topic-winserver";
-//            String kafkaTopic = "topic-ubantu-malicious";
-//            String kafkaTopic = "topic-ubantu";
-//            String kafkaTopic = "topic-test";
+            KafkaConfig kafkaConfig = new KafkaConfig();
+            String kafkaBroker = kafkaConfig.getKafkaBroker();
+            String kafkaGroupId = kafkaConfig.getKafkaGroupID();
+//            String kafkaTopic = kafkaConfig.getTopic("Topic-1");
+            String kafkaTopic = "topic-THEIA-6r";
+//            ArrayList<String> kafkaTopicList = kafkaConfig.getTopicList(1, 1);
 
-            String kafkaGroupId = "mergeAlert";
-
-            // TODO: replace deserializer with protobuf PDM deserializer
             env.getConfig().registerTypeWithKryoSerializer(PDM.LogPack.class, ProtobufSerializer.class);
             KafkaSource<PDM.LogPack> source = KafkaSource.<PDM.LogPack>builder()
                     .setBootstrapServers(kafkaBroker)
@@ -57,6 +38,7 @@ public class Main {
                     .setGroupId(kafkaGroupId)
                     .setStartingOffsets(OffsetsInitializer.earliest())
                     .setValueOnlyDeserializer(new KafkaPDMDeserializer())
+//                    .setProperty("max.poll.records", "100")
                     .build();
 
             DataStream<PDM.LogPack> logPack_stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
@@ -64,35 +46,10 @@ public class Main {
             event_stream = log_stream.map(PDMParser::initAssociatedEvent);
 //            event_stream.print();
 
-        }
-        else {
-            final String inputDirectory = "SystemLog/apt.log";
-            final FileSource<String> source =
-                    FileSource.forRecordStreamFormat(new TextLineInputFormat(), new Path(inputDirectory) ).build();
-            final DataStream<String> stream =
-                    env.fromSource(source, WatermarkStrategy.noWatermarks(), "file-source");
-            final DataStream<JsonElement> json_stream = stream.map(Main::convertToJson).name("json-source");
-            event_stream = json_stream.map(LocalParser::initAssociatedEvent);
-        }
-
         event_stream.keyBy(associatedEvent -> associatedEvent.hostUUID)
                 .process(new GraphAlignmentProcessFunction());
 
         // Execute the Flink job
         env.execute("Online Flink");
-    }
-
-    private static JsonElement convertToJson(String inputLine) {
-        Gson gson = new Gson();
-
-        try {
-            JsonElement jsonElement = gson.fromJson(inputLine, JsonElement.class);
-            System.out.println(gson.toJson(jsonElement));
-            return jsonElement;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
